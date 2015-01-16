e998a77e944ce0dd0b2d3863ee7bdc22fb65c221
hoangdt <hoangdt@smartosc.com>
2015-01-16 17:52:22 +0700

    a

--- js/austcare.js
+++ js/austcare.js
@@ -2,4 +2,27 @@ $(document).ready(function() {
 	$("#chat-me a.close").click(function(event) {
 		$(this).parents("#chat-me").hide('slow');
 	});
+	$(".box").each(function() {
+		var textHeight = $(this).children('.text').height();
+		var imageHeight = $(this).children('.image').height();
+		if (textHeight <= imageHeight) {
+			$(this).children('.text').css('height', imageHeight);
+		}else {
+			$(this).children('.image').css('height', textHeight);
+		}
+	});
+	$('.tabs li').click(function(){
+		$('.tabs li').removeClass('active');
+		$('.tab-content').removeClass('active');
+		$(this).addClass('active');
+		$($(this).find('a').attr("href") + '-content').addClass('active');
+	});
+	$(".select-city .text-top").click(function(event) {
+		$(this).next('ul').slideToggle('fast');
+	});
+	$(".select-city li a").click(function() {
+		var text = $(this).text();
+		$(".select-city ul").hide();
+		$(this).parents('.select-city').children('.text-top').text(text);
+	});
 });
